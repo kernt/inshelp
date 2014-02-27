749c0a4640f15014df79eb3c534f810d4fbe3328
tobkern <tobkern1980@googlemail.com>
2014-02-27 14:23:46 +0100

    Create getetherpad-lite.sh

---
+++ linux/app/internet/editor/etherpad-lite/getetherpad-lite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#
+#
+#
+#
+#
+
+#You'll need gzip, git, curl, libssl develop libraries, python and gcc.
+##For Debian/Ubuntu: 
+#apt-get install gzip git-core curl python libssl-dev pkg-config build-essential
+##For Fedora/CentOS: 
+#yum install gzip git-core curl python openssl-devel && yum groupinstall "Development Tools" 
+##For FreeBSD: 
+#portinstall node, npm, git (optional)
+
+#Additionally, you'll need node.js installed, Ideally the latest stable version, 
+#be careful of installing nodejs from apt.
