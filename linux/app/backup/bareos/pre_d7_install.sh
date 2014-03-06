e73d4e4761982c6c578ce20669e21aba341e579c
tobkern <tobkern1980@googlemail.com>
2014-03-06 12:06:23 +0100

    Update pre_d7_install.sh
    add apt-kex for Ubuntu and Debian linux.

--- linux/app/backup/bareos/pre_d7_install.sh
+++ linux/app/backup/bareos/pre_d7_install.sh
@@ -1,1 +1,8 @@
 ## install bareos on Debian 7
+#
+######################## pre Install #####################################################
+echo "http://download.bareos.org/bareos/release/latest/Debian_7.0/" > > bareos.list
+wget -q "http://download.bareos.org/bareos/release/latest/Debian_7.0/Release.key" -O- | apt-key add -
+# for Ubuntu
+# wget -q "http://download.bareos.org/bareos/release/latest/Debian_7.0/Release.key" -O- | sudo apt-key add -
+apt-get update
