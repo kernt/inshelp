b99f1436a02b5892e28476524624bd2501cbc64e
tobkern <tobkern1980@googlemail.com>
2014-03-10 15:10:41 +0100

    Create fu-rch.sh

---
+++ linux/Distribution/archlinux/fu-rch.sh
@@ -0,0 +1,10 @@
+It has been said that Unix/Linux, and even specifically, Arch Linux is:
+The operating system by developers for developers
+The Fu-rch project is a paradigm shift opposing that view:
+ For the  Users of Arch Linux   =     Fu-rch
+The Fu-rch Project ….. Making Arch User Friendly and Easy
+
+
+Source: http://sourceforge.net/projects/furcharchlinuxguiinstaller/files/fu-rch-0.9b-5-code.tar.gz/download
+
+http://sourceforge.net/projects/furcharchlinuxguiinstaller/files/Fu-rch-0.9b-5.iso/download
