e7d2e9ee8698e0dfe17d6aba7debff6bf9cbb455
tobkern <tobkern1980@googlemail.com>
2014-05-28 12:42:19 +0200

    Update install.sh

--- linux/Distribution/debian/OpenProject/install.sh
+++ linux/Distribution/debian/OpenProject/install.sh
@@ -54,62 +54,7 @@ echo " I will open the /etc/yum.repos.d/CentOS-Base.repo file"
 echo "and Edit for Preconfiguration Postgresql installation."
 
 sleep 5
-
-cat  > /etc/yum.repos.d/CentOS-Base.repo <<EOF
-# CentOS-Base.repo
-#
-# The mirror system uses the connecting IP address of the client and the
-# update status of each mirror to pick mirrors that are updated to and
-# geographically close to the client.  You should use this for CentOS updates
-# unless you are manually picking other mirrors.
-#
-# If the mirrorlist= does not work for you, as a fall back you can try the
-# remarked out baseurl= line instead.
-# 
-#
-
-[base]
-name=CentOS-$releasever - Base
-mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os
-baseurl=http://mirror.centos.org/centos/$releasever/os/$basearch/
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
-exclude=postgresql*
-
-#released updates 
-[updates]
-name=CentOS-$releasever - Updates
-mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates
-baseurl=http://mirror.centos.org/centos/$releasever/updates/$basearch/
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
-exclude=postgresql*
-
-#additional packages that may be useful
-[extras]
-name=CentOS-$releasever - Extras
-mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras
-#baseurl=http://mirror.centos.org/centos/$releasever/extras/$basearch/
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
-
-#additional packages that extend functionality of existing packages
-[centosplus]
-name=CentOS-$releasever - Plus
-mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus
-#baseurl=http://mirror.centos.org/centos/$releasever/centosplus/$basearch/
-gpgcheck=1
-enabled=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
-
-#contrib - packages by Centos Users
-[contrib]
-name=CentOS-$releasever - Contrib
-mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=contrib
-#baseurl=http://mirror.centos.org/centos/$releasever/contrib/$basearch/
-gpgcheck=1
-enabled=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+cat  > /etc/apt/sources.list.d/openproject.list <<EOF
 EOF
 
 # finish PostgreSQL installation
@@ -148,13 +93,9 @@ CREATE DATABASE openproject_development WITH ENCODING='UTF8' OWNER=openproject;
 CREATE DATABASE openproject_test WITH ENCODING='UTF8' OWNER=openproject;
 EOF
 
-
 # CREATEs not working make a file.sql for this error.
-
 \q
 exit
-
-
 ################################## Install Ruby ##########################################################
 # Enable repo
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
@@ -170,7 +111,6 @@ su openproject -c "bash -l"
 
 # Switch to users home dir or whatever directory you want to install openproject
 cd ~
-
 #Add following line into profile file of the desired user:
 
 echo "source ~/.profile" >> /home/openproject/.bash_profile
@@ -202,9 +142,7 @@ gem install bundler
 
 
 sleep 3
-
 echo "Ruby bundle Version:$(bundle --version) is installed."
-
 sleep 3
  
 ################################### Install OpenProject ##################################################
@@ -245,9 +183,7 @@ test:
 EOF
 
 
-
 # Configure email notifications (using gmail account) by creating configuration.yml in openproject/config directory
-
 cat  > configuration.yml <<EOF
 development:                         #main level
 email_delivery_method: :smtp         #main level, will not work
