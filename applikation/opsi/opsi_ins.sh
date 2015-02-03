2f574c5d4fac71a250a5ebb5e039516acc681ca1
tobkern <tobkern1980@googlemail.com>
2015-02-03 14:56:20 +0100

    Update opsi_ins.sh

--- applikation/opsi/opsi_ins.sh
+++ applikation/opsi/opsi_ins.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Example: opsi_ins.sh $#DOMAIN $TESTPW
 #
@@ -11,32 +12,49 @@
 # Name of your script.
 SCRIPTNAME=$(basename $0.sh)
 # exit code without any error
-EXIT_SUCCESS=0
+EXIT_SUCCESS="0"
 # exit code I/O failure
-EXIT_FAILURE=1
+EXIT_FAILURE="1"
 # exit code error if known
-EXIT_ERROR=2
+EXIT_ERROR="2"
 # unknown ERROR
-EXIT_BUG=10
-# Variable for optionsswitch
-#OPTFILE=""
-fbname=$(basename "$1".txt)
+EXIT_BUG="10"
+
 # simple help funktion , in this file because this function is only for the script itself. All others functions put in opsi_func.sh please.
 function usage {
         echo "Usage: $SCRIPTNAME [-h] [-v] [-o arg] file ..." >&2
                 [[ $# -eq 1 ]] && exit $1 || exit $EXIT_FAILURE
 }
+
+# Report opsi install
+touch /root/opsi.installed
+
+
 #. ./pre_opsi_ins.sh  # execute opsi_ins.sh 
-. ./opsi_func.sh     # execute opsi_ins.sh 
+. ./opsi_func.sh      # execute opsi_ins.sh 
+# make shure the login user is ROOT
 onlyroot
+
+# do install opsi services 
 opsinstall
+
+# do opsi initial configuration use once time !
+# 
 opsiconfig
+
+# do update all opsi config files function source is opsi_func.sh
 opsiconfupdate
 
+# Show your Java version
 JREVERSION="$(java -version)"
-
 opsi-admin -d task setPcpatchPassword
-
 echo "You have JAVA Version: $JREVERSION"
 echo "user update-alternatives --config java if you have any error."
 sleep 3
+
+# Install initial Products from UIB GmbH
+if [  ] then ;
+. ./opsi-produc-initial.sh
+
+
+
